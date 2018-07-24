c03ebfad10d6c6c4d27b0a17e13feceeb449dd8b
kallip <kallinpettit@gmail.com>
2018-07-24 19:14:37 +0000

    automatically backed up by learn

---
+++ practice.rb
@@ -0,0 +1,87 @@
+# def about_me
+#   puts "My name is Kalli"
+#   puts "I have three dogs"
+#   puts "I love elephants"
+#   puts "I love roller coasters"
+# end
+
+# about_me
+
+# def two_step
+#   puts "step to the left."
+#   puts "Step to the right."
+# end
+
+# two_step
+# two_step
+# two_step
+# two_step
+# two_step
+# two_step
+# two_step
+# two_step
+
+# def print_welcome
+#   puts "Welcome!"
+# end
+
+# print_welcome
+
+# def greeting
+#   puts "Hi Jane, I'm Kalli, how's your afternoon?"
+# end
+
+# greeting
+
+# local_greeting = "Shalom"
+# your_name = "Kalli"
+# my_name ="Kal"
+# time_of_day = "Life"
+
+# def greeting(local_greeting, your_name, my_name, time_of_day)
+#   puts "#{local_greeting} #{your_name}, I'm #{my_name}, how's your #{time_of_day}?"
+# end
+
+# greeting(local_greeting, your_name, my_name, time_of_day)
+
+# def greeting
+# local_greeting = "Shalom"
+# your_name = "Kalli"
+# my_name ="Kal"
+# time_of_day = "Life"
+#   puts "#{local_greeting} #{your_name}, I'm #{my_name}, how's your #{time_of_day}?"
+# end
+
+# greeting
+
+# def greeting
+#   local_greeting = "Hello"
+#   your_name = "everybody"
+#   my_name = "Kalli"
+#   time_of_day = "lunch"
+#   puts "#{local_greeting} #{your_name}, I'm #{my_name}, how was your #{time_of_day} break?"
+# end
+# greeting
+
+# def greeting(local_greeting, your_name, my_name, time_of_day)
+#   puts "#{local_greeting} #{your_name}, I'm #{my_name}, how's your #{time_of_day}?"
+# end
+
+# greeting("What's up", "Kim", "Kanye", "morning")
+# greeting("Hey", "Beyonce", "Karlie", "night")
+
+# def greeting(local_greeting, your_name, my_name, time_of_day)
+#   puts "#{local_greeting} #{your_name}, I'm #{my_name}, how's your #{time_of_day}?"
+# end
+# greeting("Hey", "Beyonce", "Karlie", "night")
+
+# def greeting(my_name)
+#   puts "Hi, I'm #{my_name}"
+# end
+# greeting("Cardi B")
+
+def greeting(my_name)
+  puts "Hi, I'm #{my_name}"
+end
+
+greeting("Fergie")
--- whats_your_name.rb
+++ whats_your_name.rb
@@ -1,2 +1,15 @@
-puts "what is your name"
-gets 
+# puts "what is your name?"
+# gets
+
+# puts "what is your name?"
+# name = gets
+# puts "Hello #{name}!"
+
+# name = "Kalli"
+# age = 19
+# puts "My name is " + name +  " I am " + age.to_s + " old"
+
+name = Kalli
+last = Pettit
+age = 19
+"My name is #{name} #{last}. I am #{age} old."
